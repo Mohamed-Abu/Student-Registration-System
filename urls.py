89e8409e65804cf0e63cb358790ed42b184e60cc
Mohamed-Abu <148414648+Mohamed-Abu@users.noreply.github.com>
2024-02-04 21:28:02 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index,name="homes"),
-    path('list/',views.list,name="list"),
-    path('otp/',views.verify,name="verifyotp"),
-]
